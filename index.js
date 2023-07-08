8cef76e79b1605a4668dae288de4b917eb0708de
Your Name <youremail@example.com>
2023-07-08 14:04:32 -0400

    Ready for git

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/login.html');
 });
 
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
+app.post('/api/data', (req, res) => {
+  console.log(req.body);
+  res.json({ message: 'Data received!' });
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000.');
 });
